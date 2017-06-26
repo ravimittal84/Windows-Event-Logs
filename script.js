ed77e7a9fcb2d1156b0c33a0ce509ee893dfd355
Mittal <Ravi.Mittal@sse.com>
2017-06-26 11:03:56 +0100

    Daily and Hourly breakdown added.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 (function () {
+    // TODO: Charts by days and hours
+
     $(document).ready(function () {
         let r = []; // Data Arrays
         let dt = $('#dt').DataTable({
@@ -37,22 +39,23 @@
                 // Otherwise assign data to r
                 r = $('#chkAdd').is(':checked') ? r.concat(d) : d;
 
-                PopulateTiles(r);
+                PopulateChartsAndTables(r);
             }
 
             fr.readAsText(files.item(0));
         });
 
         $('#showAll').on("click", function (e) {
+            $('#proc').show();
             e.preventDefault();
-            PopulateTiles(r);
+            PopulateChartsAndTables(r);
             $('#showAll').hide();
         });
 
         $('#filter').on("click", function (e) {
             e.preventDefault();
             const arr = filterByDate(r, $('#start').val(), $('#end').val());
-            PopulateTiles(arr);
+            PopulateChartsAndTables(arr);
             $('#showAll').show();
         });
 
@@ -61,7 +64,7 @@
 })();
 
 const renderD3Bar = (data) => {
-    $('svg').html('');
+    $('#c1').html('');
     var width = 400,
         height = 300;
 
@@ -69,7 +72,7 @@ const renderD3Bar = (data) => {
         .range([height, 0])
         .domain([0, d3.max(data, function (d) { return d.values.length; })]);
 
-    var chart = d3.select("svg")
+    var chart = d3.select("#c1")
         .attr("width", width)
         .attr("height", height)
         .attr("padding-left", "20px");
@@ -107,6 +110,104 @@ const renderD3Bar = (data) => {
         .attr("dy", ".75em")
         .text(function (d) { return d.values.length; });
 };
+
+const renderDailyBar = (data) => {
+    $('#c2').html('');
+    var width = 200,
+        height = 150;
+
+    var y = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, d3.max(data, function (d) { return d.values.length; })]);
+
+    var chart = d3.select("#c2")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("padding-left", "20px");
+
+    var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
+    var barWidth = width / data.length;
+
+    var bar = chart.selectAll("g")
+        .data(data)
+        .enter().append("g")
+        .attr("transform", function (d, i) { return "translate(" + i * barWidth + ",0)"; });
+
+    bar.append("rect")
+        .attr("y", function (d) { return y(d.values.length); })
+        .attr("height", function (d) { return height - y(d.values.length); })
+        .attr("width", barWidth - 1)
+        // .on("click", function (d) {
+        //     $('#dt').dataTable().fnClearTable();
+        //     $('#dt').dataTable().fnAddData(d.values);
+        //     $('#showAll').show();
+        // })
+        .on("mousemove", function (d) {
+            tooltip
+                .style("left", d3.event.pageX - 50 + "px")
+                .style("top", d3.event.pageY + 20 + "px")
+                .style("display", "inline-block")
+                .html(`Date: <b>${d.key}</b> <br> Events: <b>${d.values.length}</b>`);
+        })
+        .on("mouseout", function (d) { tooltip.style("display", "none"); });
+
+    bar.append("text")
+        .attr("x", barWidth / 2)
+        .attr("y", function (d) { return y(d.values.length) + 3; })
+        .attr("dy", ".75em")
+        .text(function (d) { return d.values.length; });
+
+}
+
+const renderHourlyBar = (data) => {
+    $('#c3').html('');
+    var width = 200,
+        height = 150;
+
+    var y = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, d3.max(data, function (d) { return d.values.length; })]);
+
+    var chart = d3.select("#c3")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("padding-left", "20px");
+
+    var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
+    var barWidth = width / data.length;
+
+    var bar = chart.selectAll("g")
+        .data(data)
+        .enter().append("g")
+        .attr("transform", function (d, i) { return "translate(" + i * barWidth + ",0)"; });
+
+    bar.append("rect")
+        .attr("y", function (d) { return y(d.values.length); })
+        .attr("height", function (d) { return height - y(d.values.length); })
+        .attr("width", barWidth - 1)
+        // .on("click", function (d) {
+        //     $('#dt').dataTable().fnClearTable();
+        //     $('#dt').dataTable().fnAddData(d.values);
+        //     $('#showAll').show();
+        // })
+        .on("mousemove", function (d) {
+            tooltip
+                .style("left", d3.event.pageX - 50 + "px")
+                .style("top", d3.event.pageY + 20 + "px")
+                .style("display", "inline-block")
+                .html(`Hours: <b>${d.key}</b> <br> Events: <b>${d.values.length}</b>`);
+        })
+        .on("mouseout", function (d) { tooltip.style("display", "none"); });
+
+    bar.append("text")
+        .attr("x", barWidth / 2)
+        .attr("y", function (d) { return y(d.values.length) + 3; })
+        .attr("dy", ".75em")
+        .text(function (d) { return d.values.length; });
+
+}
 
 const parseData = (arr) => {
     return arr.map(rs => {
@@ -147,30 +248,55 @@ const groupByInfo = (arr) => {
 
 // Group the array by "Date" field
 const groupByDate = (arr) => {
-    return arr.reduce((rv, x) => {
-        const v = new Date(x[4]);
-        let el = rv.find((r) => r && r.key === v);
-        if (el) {
-            el.values.push(x);
+    var array = _.groupBy(arr, (item) => {
+        return moment(item[4],"DD/MM/YYYY").format('DD/MM');
+    });
+
+    var unsortedArr = $.map(array, (val, i) => {
+        return {key: i, values: val};
+    });
+
+    return unsortedArr.sort((a,b) => { return b.key < a.key});
+};
+
+// Group the array by "Date" field
+const groupByHours = (arr) => {
+    var array = _.groupBy(arr, (item) => {
+        var d = parseInt( moment(item[4],"DD/MM/YYYY").format("H"));
+        if (d >= 0 && d < 3) {
+            return "00:00 - 02:59";
+        } else if (d >= 3 && d < 6) {
+            return "03:00 - 05:59";
+        } else if (d >= 6 && d < 9) {
+            return "06:00 - 08:59";
+        } else if (d >= 9 && d < 12) {
+            return "09:00 - 11:59";
+        } else if (d >= 12 && d < 15) {
+            return "12:00 - 14:59";
+        } else if (d >= 15 && d < 18) {
+            return "15:00 - 17:59";
+        } else if (d >= 18 && d < 21) {
+            return "18:00 - 20:59";
+        } else {
+            return "21:00 - 23:59";
         }
-        else {
-            rv.push({ key: v, values: [x] });
-        }
-        return rv;
-    }, []);
+    });
+
+    var unsortedArr = $.map(array, (val, i) => {
+        return {key: i, values: val};
+    });
+
+    return unsortedArr.sort((a,b) => { return b.key < a.key});
 };
 
 const filterByDate = (arr, startDate, endDate) => {
     return arr.filter(a => {
-        if (new Date(a[4]) > new Date(startDate) && new Date(a[4]) < new Date(endDate))
-            return true;
-
-        return false;
+        return (new Date(a[4]) > new Date(startDate) && new Date(a[4]) < new Date(endDate));
     })
 };
 
 //Populate Facts
-const PopulateTiles = (r) => {
+const PopulateChartsAndTables = (r) => {
     var erArray = [], warnArray = [];
     var minDate = new Date(), maxDate = new Date(1900, 1, 1);
     r.forEach(function (el) {
@@ -214,6 +340,8 @@ const PopulateTiles = (r) => {
 
     // Reset SVG for draw/redraw
     renderD3Bar(arr.slice(0, 10));
+    renderDailyBar(groupByDate(r));
+    renderHourlyBar(groupByHours(r));
 
     $('#proc').hide();
     $('#main').show();
