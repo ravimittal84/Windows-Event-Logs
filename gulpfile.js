d1dcfad0ad33ea98b18380ac6931452de02c9c98
Mittal <Ravi.Mittal@sse.com>
2017-07-27 14:53:50 +0100

    libraries downloaded instead of using cdn.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,17 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 
-gulp.task('minify', function() {
+
+gulp.task('move', function () {
+    return gulp.src(['app/*.html', 'app/images/*.*', 'app/scripts/*.*', 'app/styles/*.*'], {base: 'app/'})
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['move'], function () {
     return gulp.src('app/*.js')
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('default', ['minify'], function () {
-    return gulp.src(['app/*.html', 'app/*.css'])
         .pipe(gulp.dest('dist'));
 });
